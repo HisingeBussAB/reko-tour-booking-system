6dfcc0db20ae98599281f8817cda85e115068e88
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2016-12-23 15:09:46 +0100

    hej

--- config/config-example.php
+++ config/config-example.php
@@ -1,13 +1,11 @@
 <?php
 
-  /* Database details */
+  /* MSSQL Database details */
 
   define( 'DB_NAME', 'database_name_here' );
   define( 'DB_USER', 'username_here' );
   define( 'DB_PASSWORD', 'password_here' );
-  define( 'DB_HOST', 'localhost' );             //the SERVER path if MSSQL
-
-  define( 'DB_TYPE', 'MSSQL' );                 //either 'MSSQL' or 'mySql'
+  define( 'DB_SERVERNAME', 'localhost' );
 
 
   /* Provide some detals about your company */
--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,16 @@
 <?php
 
-  $connectionInfo = array("UID" => DB_USER, "pwd" => DB_PASSWORD, "Database" => DB_NAME, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-  $serverName = DB_SERVERNAME;
-  $conn = sqlsrv_connect($serverName, $connectionInfo);
+  if (substr( DB_SERVERNAME, 0, 4 ) === "tcp:") {
+    $connectionInfo = array("UID" => DB_USER, "pwd" => DB_PASSWORD, "Database" => DB_NAME, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  } else {
+    $connectionInfo = array("UID" => DB_USER, "pwd" => DB_PASSWORD, "Database" => DB_NAME);
+  }
+
+  $conn = sqlsrv_connect(DB_SERVERNAME, $connectionInfo);
+
+  if( !$conn) {
+    echo "Connection could not be established.<br />";
+    die( print_r( sqlsrv_errors(), true));
+  }
 
 ?>
