e5c9a2331ce169b856f3ca7901327c2f58554344
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2018-02-23 13:10:50 +0100

    lineendings

--- reko-bokningar/src/components/main-menu.js
+++ reko-bokningar/src/components/main-menu.js
@@ -16,22 +16,27 @@ class MainMenu extends Component {
     
     return (
       <div className="MainMenu d-print-none">
-        <nav className="my-4 mx-5">
-          <div className="d-flex flex-wrap justify-content-between">
-            <img src={Logo} alt="Logo" className="rounded custom-scale my-2 ml-3 mr-5" title="Till Startsida" id="mainLogo"/>
-            <button className="text-uppercase font-weight-bold btn btn-primary btn-lg custom-scale custom-wide-text my-2 mx-3">Resor &amp; Bokningar</button>
+        <nav className="my-4 mx-5 py-1">
+          <div className="d-flex flex-wrap justify-content-between my-2 py-1">
+            <img src={Logo} alt="Logo" className="rounded custom-scale my-2 mx-3" title="Till Startsida" id="mainLogo"/>
+            <button className="text-uppercase font-weight-bold btn btn-primary btn-lg custom-scale custom-wide-text my-2 mx-3 larger-btn">Resor &amp; Bokningar</button>
             <button className="text-uppercase font-weight-bold btn btn-primary btn-lg custom-scale custom-wide-text my-2 mx-3">Kalkyler</button>
             <button className="text-uppercase font-weight-bold btn btn-primary btn-lg custom-scale custom-wide-text my-2 mx-3">Utskick</button>
           </div>
-          <div className="d-flex flex-wrap justify-content-between my-4">
-            <input type="search" placeholder="Bokningsnr eller namn" style={searchStyle} className="rounded my-2 mx-3"/>
+          <div className="d-flex flex-wrap justify-content-between mt-4 pb-1 pt-2">
+            <input type="search" placeholder="Bokningsnr eller namn" style={searchStyle} className="rounded my-2 mx-3" />
             <select className="rounded my-2 mx-3">
-              <option disabled selected hidden>Visa bokningsläge</option>
-              <option>Resan kul</option>
+              <option disabled selected hidden>BOKNINGSLÄGE</option>
+
+              <option>Konstrundan i Skåne 30/3</option>
+              <option>Konstrundan i Skåne 30/3</option>
+              <option>Konstrundan i Skåne 30/3</option>
             </select>
             <select className="rounded my-2 mx-3">
-              <option disabled selected hidden>Sabbgenvägar</option>
-              <option>Resan kul</option>
+              <option disabled selected hidden>SNABBGENVÄGAR</option>
+              <option>Skapa resebokning</option>
+              <option>Registrera betalning</option>
+              <option>Registrera programbeställning</option>
             </select>
           </div>
         </nav>
