e438250bd4e6921b27e26b3255b0726923f3579e
Håkan Arnoldson <hakan@arnoldson.net>
2018-08-19 12:33:03 +0200

    Validator removed

--- api/v1/controller.php
+++ api/v1/controller.php
@@ -11,10 +11,6 @@ use RekoBooking\classes\common\Maintenance;
 use RekoBooking\classes\common\Auth;
 use RekoBooking\classes\common\Validate;
 use RekoBooking\classes\Functions;
-use RekoBooking\classes\actions\Getter;
-use RekoBooking\classes\actions\Putter;
-use RekoBooking\classes\actions\Poster;
-use RekoBooking\classes\actions\Deleter;
 
 
 class Controller {
@@ -22,21 +18,17 @@ class Controller {
   private $response;
   private $pdo;
   private $userData;
-  private $Validator;
 
   public function __construct() {
     $this->response = new Responder;
-    $this->response->AddResponse('saved',     false);
     $this->response->AddResponse('login',     false);
     $this->response->AddResponse('success',   false);
-    $this->response->AddResponse('validated', false);
     $this->response->AddResponse('response',  'Ingen uppgift utförd.');
     $this->pdo = DB::get($this->response);
     if ($this->pdo == false) {
       $this->response->AddResponse('response',  'Kritiskt fel. Databasanslutning misslyckades.');
       $this->response->Exit(500);
     }
-    $this->Validator = new Validator($response);
   }
 
   /**
@@ -72,22 +64,22 @@ class Controller {
       switch($_SERVER['REQUEST_METHOD'])
       {
         case "GET":
-          $this->modelInvoker($item, 'GET', $unvalidatedData);
+          $this->modelInvoker($item, 'get', $unvalidatedData);
         break;
       
         case "POST":
-          $this->modelInvoker($item, 'POST', $unvalidatedData);
+          $this->modelInvoker($item, 'post', $unvalidatedData);
           if (!ENV_CRON_JOB) { Maintenance::refreshSecrets($this->response, $this->pdo); }
         break;
 
         case "PUT":
-          $this->modelInvoker($item, 'PUT', $id, $unvalidatedData);
+          $this->modelInvoker($item, 'put', $id, $unvalidatedData);
           if (!ENV_CRON_JOB) { Maintenance::refreshSecrets($this->response, $this->pdo); }
           
         break;
 
         case "DELETE":
-          $this->modelInvoker($item, 'DELETE', $unvalidatedData);
+          $this->modelInvoker($item, 'delete', $unvalidatedData);
           if (!ENV_CRON_JOB) { Maintenance::refreshSecrets($this->response, $this->pdo); }
         break;
       }
@@ -96,16 +88,32 @@ class Controller {
   
   }
 
-  private function modelInvoker(string $item, string $function, $unvalidatedData) {
-    $model = "Try";
-    $function = 'Go';
-    try {
-      $REQUEST = new $model;
-      $data = $REQUEST->$function();
-      $this->response->AddResponse('response', $data);
-    } catch (\Exception $e) {
-      $this->response->AddResponse('response', 'Okänt serverfel. Kunde inte utföra årgärden. Kontakta tekniker om det här felet kvarstår.');
-      $this->response->LogError('Possible routing to unsupported model: ' . $e->getMessage(), __CLASS__);
+  private function modelInvoker(string $model, string $function, array $unvalidatedData) {
+    $model = 'RekoBooking\classes\models\\' . $model;
+    if (class_exists($model)) {
+      $REQUEST = new $model($this->response, $this->pdo);
+      $data = $REQUEST->$function($unvalidatedData);
+      if ($function == 'post' && $data != false) {
+        $this->response->AddResponse('response', 'OK');
+        http_response_code(201);
+      } 
+      if ($function == 'get' && $data != false) {
+        $this->response->AddResponse('response', $data);
+        http_response_code(200);
+      }
+      if (($function == 'delete' ||  $function == 'put') && $data != false) {
+        $this->response->AddResponse('response', 'OK');
+        http_response_code(200);
+      }
+         
+      if ($data == false) {
+        $this->response->AddResponse('response', 'ERROR');
+        http_response_code(501);
+      }
+      
+    } else {
+      $this->response->AddResponse('error', 'Okänt serverfel. Kunde inte utföra årgärden. Kontakta tekniker om det här felet kvarstår.');
+      $this->response->LogError('Possible routing to unsupported model: ' . $model, __CLASS__);
       $this->response->Exit(500);
     }
   }
@@ -164,7 +172,7 @@ class Controller {
     foreach ($secrets as $secret) {
       $returnedArray = Auth::validateJWT($refreshJWT, $secret['token']);
       if ($returnedArray['decoded']) {
-        $this->userData = $returedArray['jwt'];
+        $this->userData = $returnedArray['jwt'];
         if ($returnedArray['jwt']['client']['agent'] != $_SERVER['HTTP_USER_AGENT']) {
           $this->response->AddResponse('error', 'Webbläsaren har ändrats. Logga in igen.');
           header('WWW-Authenticate: Bearer');
