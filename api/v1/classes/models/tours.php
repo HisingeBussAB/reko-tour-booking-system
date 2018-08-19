62b86ba6fe162b54cefebff070b07ef114100825
Håkan Arnoldson <hakan@arnoldson.net>
2018-08-19 12:34:58 +0200

    Tours model frame

---
+++ api/v1/classes/models/tours.php
@@ -0,0 +1,20 @@
+<?php
+namespace RekoBooking\classes\models;
+
+use RekoBooking\classes\models\Model;
+
+class Tours extends Model {
+
+  public function get($param) {
+  }
+
+  public function post($param) {
+  }
+
+  public function put($param) {
+  }
+
+  public function delete($param) {
+  }
+
+}
